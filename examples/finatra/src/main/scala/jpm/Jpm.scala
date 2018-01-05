cdf32793a3f39ce92328ca5e7d2ee5dd5116256d
John Meagher <john.meagher@gmail.com>
2018-01-04 19:45:47 -0800

    Make sure the maven dependency resolution is really working

---
+++ examples/finatra/src/main/scala/jpm/Jpm.scala
@@ -0,0 +1,8 @@
+package jpm
+
+// Minimal thing to make sure the maven dependency setup is working
+import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.routing.HttpRouter
+
+object Jpm extends App {
+}
