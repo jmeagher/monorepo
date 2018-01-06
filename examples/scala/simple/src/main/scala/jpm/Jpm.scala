5f9bece392e7dbfe26700fbb562bc375975a7a0f
John Meagher <john.meagher@gmail.com>
2018-01-06 13:24:21 -0800

    Cleanup the simple example

--- examples/scala/simple/src/main/scala/jpm/Jpm.scala
+++ examples/scala/simple/src/main/scala/jpm/Jpm.scala
@@ -1,11 +1,5 @@
 package jpm
 
-// Minimal thing to make sure the maven dependency setup is working
-import com.twitter.finagle.http.Request
-import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.routing.HttpRouter
-import com.twitter.server.TwitterServer
-
 object Jpm extends App {
   def getNumber() = System.currentTimeMillis()
   println(s"This is from the JPM app with scala version ${util.Properties.versionString}. The number is ${getNumber()}")
