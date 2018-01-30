e8a8c3584224d114251a92c8a6ffa904355ac5bc
John Meagher <github@jmeagher.net>
2018-01-29 20:26:16 -0800

    Option for auto-running cql on docker startup (#9)

---
+++ examples/docker/cassandra/cassandra_init.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+exec > >(tee -i /cassandra-init.log)
+exec 2>&1
+
+set -xeuo pipefail
+if ls /*.cql ; then
+  for F in /*.cql ; do
+    touch $F.to_run
+  done
+
+  while ls /*.cql.to_run ; do
+    echo "Sleeping a little"
+    sleep 10s
+    for F in /*.cql.to_run ; do
+      echo "Trying to run $F"
+      cql="${F%.*}"
+      if cqlsh -f $cql ; then
+        echo "Successfully ran $cql"
+        rm $F
+      else
+        echo "Got an error running $cql will try again soon"
+      fi
+    done
+  done
+
+else
+  echo "No cql files in / to run so doing nothing"
+fi
+
+echo "Finished running the cassandra-init scripts"
