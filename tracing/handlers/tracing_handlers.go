0bf57d20cf35db58cdbf4f0a01bbc286fb24938f
John Meagher <john.meagher@gmail.com>
2018-11-11 17:17:26 -0800

    Oops, forgot to add the new file

---
+++ tracing/handlers/tracing_handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+// GlobalOpenTracingHandler wraps an existing handler with open-tracing calls using the global tracer
+func GlobalOpenTracingHandler(spanName string, wrapped http.Handler) http.Handler {
+	return OpenTracingHandler(opentracing.GlobalTracer(), spanName, wrapped)
+}
+
+// OpenTracingHandler wraps an existing handler with open-tracing calls using the tracer
+func OpenTracingHandler(tracer opentracing.Tracer, spanName string, wrapped http.Handler) http.Handler {
+	return &openTracingHandler{tracer, fmt.Sprintf("http.server.%s", spanName), wrapped}
+}
+
+type openTracingHandler struct {
+	tracer   opentracing.Tracer
+	spanName string
+	wrapped  http.Handler
+}
+
+func (f *openTracingHandler) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	var request = req
+	wireContext, err := opentracing.GlobalTracer().Extract(
+		opentracing.HTTPHeaders,
+		opentracing.HTTPHeadersCarrier(request.Header))
+	if err != nil {
+		// Do something here? Or ignore and just don't trace
+	}
+
+	if wireContext != nil {
+		serverSpan := opentracing.StartSpan(
+			f.spanName,
+			ext.RPCServerOption(wireContext))
+		defer serverSpan.Finish()
+		ctx := opentracing.ContextWithSpan(request.Context(), serverSpan)
+		request = request.WithContext(ctx)
+	}
+
+	f.wrapped.ServeHTTP(writer, request)
+}
