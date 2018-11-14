30f3f471c86d018184a2137e3f0f579f14951b85
John Meagher <john.meagher@gmail.com>
2018-11-13 21:11:33 -0800

    Add very basic e2e test for Jaeger logging

--- tracing/server/e2e_test.sh
+++ tracing/server/e2e_test.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 PORT=18088
 
 finish() {
+    echo "Stopping Jaeger server"
+    docker kill jaeger || true
+    echo ""
     echo "Stopping the server and returning $1"
     echo "Finish status: $2"
     ps ax | grep -v grep | grep flaky | awk '{print $1}' | xargs kill
@@ -15,10 +18,21 @@ if [ ! -f WORKSPACE ] ; then
   finish 1 "Error, run this from the top level monorepo folder"
 fi
 
+echo "Starting all-in-one Jaeger server"
+docker run --rm -d --name jaeger \
+  -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
+  -p 5775:5775/udp \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 14268:14268 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:1.7
+sleep 5s
+
 echo "Starting Flaky server"
 JAEGER_SERVICE_NAME=e2e_testing \
-  JAEGER_AGENT_HOST=localhost \
-  JAEGER_AGENT_PORT=14268 \
   bazel run \
     -- //tracing/server:flaky --flakepct=0.25 --port $PORT &
 
@@ -26,7 +40,7 @@ JAEGER_SERVICE_NAME=e2e_testing \
 SUCCESS=false
 for r in $(seq 3) ; do
    sleep 2s
-   if curl -H "uber-trace-id: a$RANDOM:1:0:0"  -s localhost:$PORT/ | grep "flake" ; then
+   if curl -s localhost:$PORT/ | grep "flake" ; then
      SUCCESS=true
      break
    else
@@ -42,7 +56,7 @@ echo "Check Flaky Percent"
 successes=0
 failures=0
 for r in $(seq 100) ; do
-  resp=$(curl -H "uber-trace-id: a$RANDOM:1:0:0" -s localhost:$PORT | egrep -o '(false|true)' || echo FAIL )
+  resp=$(curl -s localhost:$PORT | egrep -o '(false|true)' || echo FAIL )
   if [[ "$resp" == "true" ]] ; then
     successes=$((successes+1))
   elif [[ "$resp" == "false" ]] ; then
@@ -53,6 +67,7 @@ for r in $(seq 100) ; do
   fi
 done
 
+
 total=$((successes + failures))
 if [[ successes -lt 23 ]] ; then
   finish 1 "Got an unexpectedly low number of successes, but got $successes"
@@ -60,6 +75,15 @@ elif [[ failures -lt 72 ]] ; then
   finish 1 "Got an unexpectedly low number of failures, but got $failures"
 elif [[ total -ne 100 ]] ; then
   finish 1 "Should have gotten exactly 100 responses, but got $total"
+fi
+
+echo "Validate that tracing is really working"
+ID=$RANDOM
+curl -H "Uber-Trace-Id: $ID:$ID:0:3" localhost:$PORT
+sleep 1s
+curl -v http://localhost:16686/api/traces/$ID | grep "\"traceID\":\"$ID\"" && FOUND=true || FOUND=false
+if [[ $FOUND != true ]] ; then
+  finish 1 "Did not find the trace in Jaeger"
 fi
 
 finish 0 "Test looks successful"
