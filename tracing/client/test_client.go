3d1e8080300a0320a1ef98418d02a4589103c57c
John Meagher <john.meagher@gmail.com>
2018-11-25 17:22:53 -0800

    Add parallelization options to the client

--- tracing/client/test_client.go
+++ tracing/client/test_client.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/jmeagher/monorepo/tracing/jaeger"
@@ -19,6 +20,7 @@ func main() {
 	port := flag.Int("port", 8080, "port to listen on")
 	host := flag.String("host", "localhost", "host to connect to")
 	requestCount := flag.Int("requests", 10, "number of test requests to make")
+	parallelism := flag.Int("parallelism", 1, "requests to make in parallel")
 	expectedSuccessRate := flag.Float64("expected_sr", -1, "The expected success rate, if set between 0.0 and 1.0 this will cause the process to fail if the success rate is not close to this setting")
 	expectedSuccessRateThreshold := flag.Float64("sr_threshold", 0.05, "Sets the range +- the expected_sr that is acceptable for the expected_sr check")
 	flag.Parse()
@@ -29,31 +31,57 @@ func main() {
 	min := *expectedSuccessRate - *expectedSuccessRateThreshold
 	max := *expectedSuccessRate + *expectedSuccessRateThreshold
 
-	status, success := runTest(url, *requestCount, min, max)
-	if !success {
+	status, success := runTest(url, *requestCount, min, max, *parallelism)
+	if success {
+		log.Println(success)
+	} else {
 		log.Fatalln(status)
 	}
 }
 
-func runTest(url string, count int, minSr float64, maxSr float64) (string, bool) {
+func runTest(url string, count int, minSr float64, maxSr float64, parallelism int) (string, bool) {
 	var closer, err = jaeger.Init()
 	if err != nil {
 		log.Fatal("Jaeger Init Fail: ", err)
 	}
 	defer closer.Close()
-	time.Sleep(100 * time.Millisecond)
 
-	var successes = 0
-	var failures = 0
-	for i := 0; i < count; i++ {
-		var response, err = doTest(i, url)
-		if err != nil {
-			log.Printf("Test error: %s", err.Error())
-			failures += 1
-		} else if response == nil || response.StatusCode >= 300 {
-			failures += 1
+	requests := make(chan int, parallelism)
+	go func() {
+		for i := 0; i < count; i++ {
+			requests <- i
+		}
+		close(requests)
+	}()
+
+	var wg sync.WaitGroup
+	results := make(chan bool, count)
+	wg.Add(count)
+
+	for i := 0; i < parallelism; i++ {
+		go asyncTest(requests, url, results, &wg)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	// All done!
+	case <-time.After(50 * time.Second):
+		log.Println("Warning: Hit timeout and did not get full results")
+	}
+
+	close(results)
+	var successes, failures int
+	for i := range results {
+		if i {
+			successes++
 		} else {
-			successes += 1
+			failures++
 		}
 	}
 
@@ -65,6 +93,22 @@ func runTest(url string, count int, minSr float64, maxSr float64) (string, bool)
 		return fmt.Sprintf("Success rate was %v which is more than the max of %v\n", successRate, maxSr), false
 	}
 	return fmt.Sprintf("Got a good success rate of %v\n", successRate), true
+}
+
+func asyncTest(runNum chan int, url string, result chan bool, wg *sync.WaitGroup) {
+
+	for i := range runNum {
+		var response, err = doTest(i, url)
+		var success = true
+		if err != nil {
+			log.Printf("Test error: %s", err.Error())
+			success = false
+		} else if response == nil || response.StatusCode >= 300 {
+			success = false
+		}
+		result <- success
+		wg.Done()
+	}
 }
 
 func doTest(testRun int, url string) (*http.Response, error) {
