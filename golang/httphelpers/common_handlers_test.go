733fe718c3489d50450ebaa80bbfea62779c81ce
John Meagher <github@jmeagher.net>
2019-07-06 13:43:10 -0700

    Add a handler to load balance between backends (#67)

--- golang/httphelpers/common_handlers_test.go
+++ golang/httphelpers/common_handlers_test.go
@@ -8,17 +8,14 @@ import (
 	"testing"
 )
 
-// TestRandomSplit functionality
-func TestRandomSplit(t *testing.T) {
-	h1 := StaticHandler("test", "plain/text", 200)
-	h2 := StaticHandler("test", "plain/text", 400)
-	h := RandomSplitHandler(0.9, h1, h2)
+func testSplitHandler(t *testing.T, split http.Handler, expectedSuccessRatio float32) {
 	successes := 0
 	failures := 0
-	for i := 0; i < 1000; i++ {
+	count := 1000
+	for i := 0; i < count; i++ {
 		r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
 		rr := httptest.NewRecorder()
-		h.ServeHTTP(rr, r)
+		split.ServeHTTP(rr, r)
 		switch rr.Code {
 		case 200:
 			successes++
@@ -28,13 +25,30 @@ func TestRandomSplit(t *testing.T) {
 			t.Error(fmt.Sprintf("Got an unrecognized response code %d", rr.Code))
 		}
 	}
-	if (successes + failures) != 1000 {
-		t.Error("Didn't get a full set of responses")
+	if (successes + failures) != count {
+		t.Errorf("Didn't get a full set of responses, expected %v, but got %v", count, successes+failures)
 	}
-	if successes < 880 {
-		t.Error("Didn't get enough successful responses")
+
+	min := int(float32(count) * expectedSuccessRatio * 0.95)
+	max := int(float32(count) * expectedSuccessRatio * 1.05)
+	if successes < min {
+		t.Errorf("Didn't get enough successful responses, got %v, but expected at least %v", successes, min)
 	}
-	if successes > 920 {
-		t.Error("Didn't get enough successful responses")
+	if successes > max {
+		t.Errorf("Got too many successful responses, got %v, but expected less than %v", successes, max)
 	}
+}
+
+func TestRandomSplitHandler(t *testing.T) {
+	h1 := StaticHandler("test", "plain/text", 200)
+	h2 := StaticHandler("test", "plain/text", 400)
+	h := RandomSplitHandler(0.9, h1, h2)
+	testSplitHandler(t, h, 0.9)
+}
+
+func TestEvenSplitHandler(t *testing.T) {
+	h1 := StaticHandler("test", "plain/text", 200)
+	h2 := StaticHandler("test", "plain/text", 400)
+	h := EvenSplitHandler(h1, h2)
+	testSplitHandler(t, h, 0.5)
 }
