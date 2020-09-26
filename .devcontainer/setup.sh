82d65aff327aef6093318aea07e343f59bd35071
John Meagher <github@jmeagher.net>
2020-09-25 20:09:56 -0700

    Add dotfiles

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,7 +8,16 @@ cd $CODESPACES_TMP
 
 # Extra base packages
 apt install -y python3-distutils python3-dev python3-pip \
-  jq entr
+  jq entr tmux vim
+
+# My dotfiles setup
+if [ ! -e dotfiles ] ; then
+  git clone https://github.com/jmeagher/dotfiles.git
+  mv ~/.zshrc ~/.zshrc.orig
+  mv ~/.bashrc ~/.bashrc.orig
+  mv ~/.gitconfig ~/.gitconfig_local
+  ./setup.sh
+fi
 
 # Install and setup ibazel
 # export PATH=$PATH:$CODESPACES_TMP/bazel-watcher/bazel-bin/ibazel/linux_amd64_pure_stripped
